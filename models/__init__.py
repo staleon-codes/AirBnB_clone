90ab4d463ea6a2989f7148cd05438bc411985c7c
Peter McMillan <132352243+Steezcool@users.noreply.github.com>
2023-11-12 22:34:44 +0200

    Update

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
-"""Initializes the package."""
-
+"""Initializes the package"""
 from models.engine.file_storage import FileStorage
-
-def main():
-    """Main function."""
-    # Create an instance of FileStorage
-    storage = FileStorage()
-
-    # Reload data if available
-    storage.reload()
-
-if __name__ == "__main__":
-    main()
+storage = FileStorage()
+storage.reload()
