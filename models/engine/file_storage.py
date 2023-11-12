d75454f4cfd97978bf4d2d8dbd1f372b3cf49008
Peter McMillan <132352243+Steezcool@users.noreply.github.com>
2023-11-12 22:50:35 +0200

    Update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,95 +1,71 @@
-#!/usr/bin/python3
-"""Module for FileStorage class."""
-import datetime
+#!usr/bin/python3
+
+""" This is the File storage model handling storage and serialization """
 import json
-import os
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
-class FileStorage:
+class FileStorage():
 
-    """Class for storing and retrieving data"""
+    """
+    This represents FileStorage class
+    """
+
     __file_path = "file.json"
     __objects = {}
+    __models = {
+        'User': User,
+        'BaseModel': BaseModel,
+        'State': State,
+        'City': City,
+        'Amenity': Amenity,
+        'Place': Place,
+        'Review': Review
+    }
 
     def all(self):
-        """returns the dictionary __objects"""
-        return FileStorage.__objects
+        """
+        This Returns all objects in BaseModel class representing format
+        """
+        return self.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        """
+        This is to Add obj to objects
+        """
+        self._objects[f"{obj.class.name_}.{obj.id}"] = obj
 
     def save(self):
-        """ serializes __objects to the JSON file (path: __file_path)"""
-        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
-            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
-            json.dump(d, f)
-
-    def classes(self):
-        """Returns a dictionary of valid classes and their references"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-
-        classes = {"BaseModel": BaseModel,
-                   "User": User,
-                   "State": State,
-                   "City": City,
-                   "Amenity": Amenity,
-                   "Place": Place,
-                   "Review": Review}
-        return classes
+        """"
+        This Serializes objects in __objects to json objects and
+        saves them in file.json file format
+        """
+        json_objs = {}
+        for key, val in self.__objects.items():
+            json_objs[key] = val.to_dict()
+        with open(self.__file_path, "w", encoding='utf-8') as f:
+            json.dump(json_objs, f, indent=4)
 
     def reload(self):
-        """Reloads the stored objects"""
-        if not os.path.isfile(FileStorage.__file_path):
-            return
-        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
-            obj_dict = json.load(f)
-            obj_dict = {k: self.classes()[v["__class__"]](**v)
-                        for k, v in obj_dict.items()}
-            # TODO: should this overwrite or insert?
-            FileStorage.__objects = obj_dict
+        """
+        to Deserializes the JSON objects in file.json
+        """
+        try:
+            with open(self.__file_path, "r", encoding='utf-8') as f:
+                json_objs = json.load(f)
 
-    def attributes(self):
-        """Returns the valid attributes and their types for classname"""
-        attributes = {
-            "BaseModel":
-                     {"id": str,
-                      "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime},
-            "User":
-                     {"email": str,
-                      "password": str,
-                      "first_name": str,
-                      "last_name": str},
-            "State":
-                     {"name": str},
-            "City":
-                     {"state_id": str,
-                      "name": str},
-            "Amenity":
-                     {"name": str},
-            "Place":
-                     {"city_id": str,
-                      "user_id": str,
-                      "name": str,
-                      "description": str,
-                      "number_rooms": int,
-                      "number_bathrooms": int,
-                      "max_guest": int,
-                      "price_by_night": int,
-                      "latitude": float,
-                      "longitude": float,
-                      "amenity_ids": list},
-            "Review":
-            {"place_id": str,
-                         "user_id": str,
-                         "text": str}
-        }
-        return attributes
+            for key, val in json_objs.items():
+                constractor = val["_class_"]
+                if val["_class"] in self._models.keys():
+                    self._objects[key] = self.models[val["class_"]](
+                        **val)
+        except FileNotFoundError:
+            pass
+        except Exception as e:
+            pass
